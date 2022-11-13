14691a68c8715b3b0f65b054c440042595aade0c
Krong1997 <a631953720@gmail.com>
2022-11-13 19:33:39 +0800

    refactor: split orm and modules

--- packages/my-app/src/module/user.ts
+++ packages/my-app/src/module/user.ts
@@ -1,40 +1,4 @@
-import { DataTypes, Optional, Model } from 'sequelize';
-import { sequelize } from './pg';
-
-type UserAttributes = {
-  id: number;
-  Name: string;
-  Password: string;
-  Alias: string;
-};
-
-type UserCreationAttributes = Optional<UserAttributes, 'id'>;
-
-const User = sequelize.define<Model<UserAttributes, UserCreationAttributes>>(
-  'User',
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    Name: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    Password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    Alias: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    tableName: 'User',
-  }
-);
+import { User } from "./orm/user";
 
 export async function findAllUser() {
   try {
